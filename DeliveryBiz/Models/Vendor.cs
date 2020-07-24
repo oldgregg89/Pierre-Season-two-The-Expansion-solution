78735cbfb9dc0eb41bb2587ebd29fe7d71e32453
Ian Gregg <iangregg188@gmail.com>
2020-07-24 12:11:16 -0700

    adding everying for the first push of the day.

--- DeliveryBiz/Models/Vendor.cs
+++ DeliveryBiz/Models/Vendor.cs
@@ -12,7 +12,9 @@ namespace DeliveryBiz.Models
     public Vendor(string vendorName)
     {
       Name = vendorName;
+      Id = _instances.Count;
       _instances.Add(this);
+      Order = new List<Order>{};
     }
     public static void ClearAll()
     {
@@ -21,6 +23,10 @@ namespace DeliveryBiz.Models
     public static List<Vendor> GetAll()
     {
       return _instances;
+    }
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
