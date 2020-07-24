18aaf8d1070d199aafd25d0c6cb4e1812f1960b2
Ian Gregg <iangregg188@gmail.com>
2020-07-24 14:34:41 -0700

    working on the Vendor Controller.

--- DeliveryBiz/Controllers/VendorController.cs
+++ DeliveryBiz/Controllers/VendorController.cs
@@ -1,3 +1,29 @@
+using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using DeliveryBiz.Models;
-using System.Collections.Generic;
+
+namespace DeliveryBiz.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendor")]
+    public ActionResult Create(string vendorName);
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+
+    }
+  }
+}
